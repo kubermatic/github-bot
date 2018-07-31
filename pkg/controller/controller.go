811641902e45309be0d7034cfb14ae60d660c6fe
Alvaro Aleman <alv2412@googlemail.com>
2018-07-31 22:24:01 +0200

    Fetch comment

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -1,7 +1,11 @@
 package controller
 
 import (
+	"context"
 	"log"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/google/go-github/github"
 )
@@ -19,5 +23,33 @@ func (c *Controller) HandleNotification(notification *github.Notification) error
 		log.Println("Dropping message because its not a PR...")
 		return nil
 	}
+
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancelFunc()
+
+	body, err := c.getComment(ctx, *notification.Repository, notification.Subject.GetLatestCommentURL())
+	if err != nil {
+		return err
+	}
+	log.Printf("Got message body: %s", body)
 	return nil
+}
+
+func (c *Controller) getComment(ctx context.Context, repo github.Repository, commentUrl string) (string, error) {
+	commentID, err := getCommentIDFromCommentUrl(commentUrl)
+	if err != nil {
+		return "", err
+	}
+
+	repoComment, _, err := c.client.Issues.GetComment(ctx, repo.GetOwner().GetLogin(), repo.GetName(), commentID)
+	if err != nil {
+		return "", err
+	}
+	return repoComment.GetBody(), nil
+}
+
+func getCommentIDFromCommentUrl(commentUrl string) (int, error) {
+	commentURLSplitted := strings.Split(commentUrl, "/")
+	commentIDAsString := commentURLSplitted[len(commentURLSplitted)-1]
+	return strconv.Atoi(commentIDAsString)
 }
